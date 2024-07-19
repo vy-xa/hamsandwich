146c2b5f91693526b3f8bff90fd131d1f9b09bfb
vyxa <159471891+vy-xa@users.noreply.github.com>
2024-07-19 16:37:47 +0100

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('showCheatsButton').addEventListener('click', function() {
-    document.getElementById('home').style.display = 'none';
-    document.getElementById('cheats').style.display = 'block';
-});

77cec380860dd8cae899a8bdbea05fb1c9c776b2
vyxa <159471891+vy-xa@users.noreply.github.com>
2024-07-19 15:42:16 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('showCheatsButton').addEventListener('click', function() {
+    document.getElementById('home').style.display = 'none';
+    document.getElementById('cheats').style.display = 'block';
+});
